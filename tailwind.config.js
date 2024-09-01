318ff8a4bb19ac2173d3546da1cb2a2e205b1d2f
AnushkaShendge <anushkashendge5@gmail.com>
2024-09-01 17:01:27 +0530

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,12 @@ module.exports = {
         card: "0px 35px 120px -15px #211e35",
       },
       screens: {
-        xs: "450px",
+        xs: "450px",   // Small phones
+        sm: "640px",   // Mobile phones
+        md: "768px",   // iPads
+        lg: "1024px",  // Smaller laptops
+        xl: "1280px",  // Laptops 15-16 inches
+        "2xl": "1536px", // Larger screens
       },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
